bdfb4c422c59e742a92c91a6f7e66cde0f296746
amurto <amurtobasu@gmail.com>
2020-03-14 15:37:51 +0530

    Updates

---
+++ precache-manifest.e7c8325c660c59abf6e67a99bdb6b445.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bfd5fe7ddf27354e33e61ac8f11a393",
+    "url": "/react-india-region-selector/index.html"
+  },
+  {
+    "revision": "8d8611398edabae13660",
+    "url": "/react-india-region-selector/static/css/main.6b35f3b2.chunk.css"
+  },
+  {
+    "revision": "2bf39774dfcb923e754a",
+    "url": "/react-india-region-selector/static/js/2.85d27c00.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/react-india-region-selector/static/js/2.85d27c00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d8611398edabae13660",
+    "url": "/react-india-region-selector/static/js/main.1a986f12.chunk.js"
+  },
+  {
+    "revision": "cad3dfc4a6abf0197009",
+    "url": "/react-india-region-selector/static/js/runtime-main.b2fcfe6c.js"
+  }
+]);
